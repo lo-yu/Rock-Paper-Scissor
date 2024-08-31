a4722351f99938aed0ba3bb64138289b4edb39e9
Lobsang Yungdung <[10lobsangyungdung10@gmail.com]>
2024-08-31 19:22:27 +0530

    Write logic to get computer choice

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+function getComputerChoice() {
+    let random = Math.floor(Math.random() * 100);
+    // console.log(random);
+    if (random >= 0 && random <= 33){
+        return "rock";
+    }
+    else if (random >= 34 && random <= 66) {
+        return "paper";
+    }
+    else {
+        return "scissor";
+    }
+}
+// console.log(getComputerChoice());
